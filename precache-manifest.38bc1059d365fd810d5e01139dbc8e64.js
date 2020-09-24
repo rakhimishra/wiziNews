d877e1811294adbd1c9b45fffdf87ab30b631f4b
rakhimishra <mishrarakhi21@gmail.com>
2020-09-24 23:49:05 +0530

    Updates

---
+++ precache-manifest.38bc1059d365fd810d5e01139dbc8e64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "587e93ff7c747819d858beb0f0af7488",
+    "url": "/wizinews/index.html"
+  },
+  {
+    "revision": "47cf6c05cc2a9509e35f",
+    "url": "/wizinews/static/css/main.0c9b0068.chunk.css"
+  },
+  {
+    "revision": "85f8ff9a813e0e869dca",
+    "url": "/wizinews/static/js/2.d7316334.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wizinews/static/js/2.d7316334.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47cf6c05cc2a9509e35f",
+    "url": "/wizinews/static/js/main.f95fe397.chunk.js"
+  },
+  {
+    "revision": "d097e59fd553b7fd5ad5",
+    "url": "/wizinews/static/js/runtime-main.700f5382.js"
+  },
+  {
+    "revision": "c10c6d6d8ee3fd097f627869a1757658",
+    "url": "/wizinews/static/media/react.c10c6d6d.svg"
+  }
+]);
